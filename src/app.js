255c078ecc59ea5a80d3c0b326419cbcb03284d7
ThanhDat <caothanhdat02003@gmail.com>
2026-04-02 23:08:59 +0700

    add domain CORS

--- src/app.js
+++ src/app.js
@@ -44,7 +44,7 @@ const appOrigin = [
   "https://shop-basagi.com",
   "http://localhost:3000",
   "http://127.0.0.1:3000",
-  "https://dqpbp-fe.vercel.app/",
+  "https://dqpbp-fe.vercel.app",
 ];
 
 if (process.env.NODE_ENV === "development") {
