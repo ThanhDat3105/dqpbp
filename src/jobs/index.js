f4c1e98a2c22f6da3bc1fa7449f296356bea2c29
ThanhDat <caothanhdat02003@gmail.com>
2026-06-07 17:23:52 +0700

    update code

--- src/jobs/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-const { startDailyDigestJob } = require("./dailyDigest.job");
-
-const startJobs = () => {
-  startDailyDigestJob();
-};
-
-module.exports = { startJobs };
--- src/validations/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-module.exports = {
-  activityValidation: require("./activity.validation"),
-  authValidation: require("./auth.validation"),
-  activityTaskValidation: require("./activity-task.validation"),
-  activityTemplateValidation: require("./activity-template.validation"),
-};
